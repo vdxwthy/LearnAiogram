1ff767aed6faa943b74a2265e6fdba4ced2222d1
vdxwthy <wxnnvdie@gmail.com>
2025-08-01 20:24:33 +0500

    add answer for start command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -8,8 +10,13 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message(Command('start'))
+async def start(message: Message):
+  await message.answer('Hi, this is the bot I use to learn Aiogram v3')
+
 async def main(): 
     await dp.start_polling(bot)
+    
     
 if __name__ == '__main__':
   asyncio.run(main())
