fc02dbefcd8916494a1fa4be2b13b517065356a5
vdxwthy <wxnnvdie@gmail.com>
2025-08-01 20:31:32 +0500

    add magic filter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 from dotenv import load_dotenv
@@ -13,10 +13,14 @@ dp = Dispatcher()
 @dp.message(Command('start'))
 async def start(message: Message):
   await message.answer('Hi, this is the bot I use to learn Aiogram v3')
+  
+  
+@dp.message(F.text == 'Hello') # You can filter by anything (e.g. F.photo, F.video, F.voice, F.sticker)
+async def hello(message: Message):
+    await message.answer('Hello! How are you?')
 
 async def main(): 
     await dp.start_polling(bot)
-    
-    
+       
 if __name__ == '__main__':
   asyncio.run(main())
